c1e316c8544f15446a292ca73133261018005d8f
Retzudo <christoph.willinger@gmail.com>
2015-03-16 20:49:19 +0100

    Comments, custom city retrieving via URL

--- app.py
+++ app.py
@@ -1,37 +1,56 @@
 #!./env/bin/python3
-import urllib.request
 import os
+import urllib.request
+from bs4 import BeautifulSoup
+from flask import abort
 from flask import Flask
 from flask import render_template
-from bs4 import BeautifulSoup
 
-STATE = 'oberoesterreich'
+STATE = os.getenv('STATE', 'oberoesterreich')
 CITY = os.getenv('CITY', 'Ried im Innkreis')
 URL = 'http://zamg.ac.at/cms/de/wetter/wetterwerte-analysen/{state}'
 
 app = Flask(__name__)
 
 
-def get_weather_html():
-    response = urllib.request.urlopen(URL.format(state=STATE))
+def get_weather_html(state=STATE):
+    """Get the HTML from ZAMG or throw a 404 error."""
+    try:
+        response = urllib.request.urlopen(URL.format(state=state))
+    except HTTPError:
+        abort(404)
+
     return response.read()
 
 
-def get_weather():
-    html = BeautifulSoup(get_weather_html())
-    row = html.find('a', text=CITY).parent.parent
+def get_weather(state=STATE, city=CITY):
+    """Find the table row with the requested city name or go 404."""
+    html = BeautifulSoup(get_weather_html(state=state))
+    link = html.find('a', text=city)
+    if link == None:
+        abort(404)
+
+    row = link.parent.parent
+
+    # Convert "00.0 °C" to a float 0.0
     temp_string = row.contents[2].string
     temp = float(temp_string[:temp_string.find('°')].strip())
+    # Strip the % from the humidity text
     hum_string = row.contents[3].string
     humidity = float(hum_string[:hum_string.find('%')].strip())
+    # Remove the % from the 'sunlight' string
     sun_string = row.contents[7].string
     sun = float(sun_string[:sun_string.find('%')].strip())
+
     return (temp, humidity, sun)
 
 
 @app.route('/')
-def index():
-    temperature, humidity, sun = get_weather()
+@app.route('/<string:state>/<string:city>')
+def index(state=STATE, city=CITY):
+    state = state.lower()
+    city = city.replace('+', ' ')
+    temperature, humidity, sun = get_weather(state=state, city=city)
     return render_template('index.html', temperature=temperature, humidity=humidity, sun=sun)
 
 
